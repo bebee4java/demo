d213b1735d505c98e0c21932c9b1abd28c703a13
bebee <253824369@qq.com>
2017-10-10 23:17:59 +0800

    提交demo

---
+++ test/src/test/java/Test.java
@@ -0,0 +1,11 @@
+import org.apache.commons.lang3.StringUtils;
+
+
+/**
+ * Created by sgr on 2017/9/26.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.join(new Object[]{"a","b","c"},'|'));
+    }
+}
