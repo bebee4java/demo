b686e68f63de1ed121fb1b7891fe4ed41e666351
bebee <253824369@qq.com>
2017-11-01 18:11:10 +0800

    提交

--- test/src/test/java/Test.java
+++ test/src/test/java/Test.java
@@ -1,15 +1,8 @@
-import org.apache.commons.lang3.StringUtils;
-
-
 /**
  * Created by sgr on 2017/9/26.
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println(StringUtils.join(new Object[]{"a","b","c"},'|'));
-
-        String b = "20171025";
-        String a = "20171026";
-        System.out.println(Integer.parseInt(a) - Integer.parseInt(b) > 1);
+        System.out.println(" ".matches("(^[ ]*)"));
     }
 }
