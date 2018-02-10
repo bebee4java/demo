0187a2d433558612cff568f2ed87367639d40302
bebee <253824369@qq.com>
2018-02-10 15:41:41 +0800

    提交spark算子代码

---
+++ spark-java/src/main/java/com/java/spark/operator/CountByKeyOperator.java
@@ -0,0 +1,35 @@
+package com.java.spark.operator;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by sgr on 2018/2/8/008.
+ */
+public class CountByKeyOperator {
+    public static void main(String[] args) {
+        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("CountByKeyOperator");
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+
+        List<Tuple2<String,String>> list = Arrays.asList(
+                new Tuple2<String, String>("浙江","杭州"),
+                new Tuple2<String, String>("江苏","南京"),
+                new Tuple2<String, String>("浙江","金华"),
+                new Tuple2<String, String>("江苏","苏州"),
+                new Tuple2<String, String>("北京","北京")
+        );
+        JavaPairRDD<String,String> javaPairRDD = sparkContext.parallelizePairs(list);
+        //按key count action操作 有shuffle过程
+        Map<String,Long> result =  javaPairRDD.countByKey();
+        for (Map.Entry<String,Long> entry : result.entrySet()){
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+        sparkContext.close();
+    }
+}
